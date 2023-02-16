3406a17225acf1aedd033aa6e40022513ffcb9c1
jiwom <jeromesarmiento.dev@gmail.com>
2023-02-16 19:21:44 +0800

    fix: Home redirect plus partial products crud

--- app/Http/Livewire/ProductLivewire.php
+++ app/Http/Livewire/ProductLivewire.php
@@ -2,12 +2,74 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Product;
 use Livewire\Component;
 
 class ProductLivewire extends Component
 {
+    public $name, $category, $description, $id;
+    public $updateMode = false;
+   
     public function render()
     {
         return view('livewire.product-livewire');
+    }
+
+    public function store()
+    {
+        $validatedDate = $this->validate([
+            'name' => 'required',
+            'category' => 'required',
+            'description' => 'required',
+        ]);
+  
+        Product::create($validatedDate);
+  
+        session()->flash('message', 'Product Created Successfully.');
+  
+        $this->resetInputFields();
+    }
+
+    public function edit($id)
+    {
+        $product = Product::findOrFail($id);
+        $this->id = $id;
+        $this->category = $product->category;
+        $this->description = $product->description;
+  
+        $this->updateMode = true;
+    }
+
+    public function cancel()
+    {
+        $this->updateMode = false;
+        $this->resetInputFields();
+    }
+
+    public function update()
+    {
+        $validatedDate = $this->validate([
+            'name' => 'required',
+            'category' => 'required',
+            'description' => 'required',
+        ]);
+  
+        $product = Product::find($this->post_id);
+        $product->update([
+            'name' => $this->name,
+            'category' => $this->category,
+            'description' => $this->description,
+        ]);
+  
+        $this->updateMode = false;
+  
+        session()->flash('message', 'Product Updated Successfully.');
+        $this->resetInputFields();
+    }
+
+    public function delete($id)
+    {
+        Product::find($id)->delete();
+        session()->flash('message', 'Product Deleted Successfully.');
     }
 }
--- resources/views/livewire/product-livewire.blade.php
+++ resources/views/livewire/product-livewire.blade.php
@@ -2,6 +2,16 @@
     @push('breadcrumb-main')
         Product List
     @endpush
+    @if (session()->has('message'))
+        <div class="alert alert-success">
+            {{ session('message') }}
+        </div>
+    @endif
+    @if($updateMode)
+        @include('livewire.update')
+    @else
+        @include('livewire.create')
+    @endif
     <div class="row">
         <div class="col-md-12">
             <div class="card">
