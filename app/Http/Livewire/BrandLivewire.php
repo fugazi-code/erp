19ddb0c00a5fc6e52dcfaafffeebb2d641a3809f
Renier <17608701+fugazi-code@users.noreply.github.com>
2023-03-18 05:33:28 +0800

    update

--- app/Http/Livewire/BrandLivewire.php
+++ app/Http/Livewire/BrandLivewire.php
@@ -2,10 +2,60 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Brand;
 use Livewire\Component;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 
 class BrandLivewire extends Component
 {
+    use LivewireAlert;
+    
+    public array $detail = [];
+    protected $listeners = ['bind'];
+
+    public function bind($id)
+    {
+        $this->detail = Brand::find($id)->toArray();
+        $this->dispatchBrowserEvent('open-modal-crudModal');
+    }
+
+    public function update()
+    {
+        Brand::updateOrCreate(
+            ['id' => $this->detail['id']],
+            ['name' => $this->detail['name']]
+        );
+
+        session()->flash('message', 'Updated Successfully.');
+        $this->detail = [];
+        $this->emit('refreshDatatable');
+        $this->dispatchBrowserEvent('close-modal-crudModal');
+    }
+
+    public function delete()
+    {
+        Brand::find($this->detail['id'])->delete();
+
+        $this->emit('refreshDatatable');
+        session()->flash('message', 'Deleted Successfully.');
+        $this->dispatchBrowserEvent('close-modal-crudModal');
+        $this->detail = [];
+    }
+
+    public function store()
+    {
+        Brand::create([
+            'name' => $this->detail['name'],
+            'description' => 'description.',
+            'created_by' => auth()->id()
+        ]);
+
+        $this->detail = [];
+        $this->emit('refreshDatatable');
+        $this->alert('success', 'Added Successfully!');
+        $this->dispatchBrowserEvent('close-modal-crudModal');
+    }
+
     public function render()
     {
         return view('livewire.brand-livewire');
--- resources/views/livewire/brand-livewire.blade.php
+++ resources/views/livewire/brand-livewire.blade.php
@@ -6,9 +6,14 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-body">
+                    <div class="mb-3">
+                        <button class="btn btn-primary" wire:click='$set("detail", [])' data-bs-toggle="modal"
+                        data-bs-target="#crudModal">Add Item</button>
+                    </div>
                     <livewire:table.brand-table />
                 </div>
             </div>
         </div>
     </div>
+    @include('livewire.partials.item-form-modal')
 </div>
